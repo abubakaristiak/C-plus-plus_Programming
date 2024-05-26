bdf6521a3f27f3bdf95bc33fd032e4774640327d
Abu Bakar Istiak <151659711+abubakaristiak@users.noreply.github.com>
2024-05-26 21:28:59 +0600

    Module - 05 (Strings)

---
+++ _08_stringstream.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    getline(cin,s);
+    stringstream ss(s);
+    string word;
+    int cnt = 0;
+    while(ss >> word)
+    {
+        cout << word << endl;
+        cnt++;
+    }
+    cout<<"Total words: "<< cnt <<endl;
+    return 0;
+}
